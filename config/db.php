267a69ecc75da5b462d7ba0aa68da6c533f81d90
RAFAELDEOLIVEIRABAHIA <rafael.bahia@email.br>
2021-10-04 05:36:37 -0300

    first commit - App Heroku

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=organizacao_defesa',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=usr5duio2satt5pg',
+    'username' => 'hy8qvzw452vi5lyb',
+    'password' => 'bzzy6w4l1nem798f',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
