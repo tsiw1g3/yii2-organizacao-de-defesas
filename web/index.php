7238fa2115fba2c1b17ce02fb688b8d6150ed20f
bfs07 <bfs.07@hotmail.com>
2021-10-05 20:24:09 -0300

    Update Auth

--- web/index.php
+++ web/index.php
@@ -6,7 +6,8 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept');
+header('Authorization: *');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
