730b048d6313d3f26510798d4f390f4e0d9d7c9b
João Pedro Brito Silva <contato.joaocomp@gmail.com>
2023-08-07 17:27:00 -0300

    fix: changes default database location

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost:3306;dbname=organizacao_defesa',
+    'dsn' => 'mysql:host=host.docker.internal:3306;dbname=organizacao_defesa',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
