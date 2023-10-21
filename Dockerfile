76bd1226acba951a90b2db41426fd67db2a5c6db
João Pedro Brito Silva <contato.joaocomp@gmail.com>
2023-10-21 12:00:17 -0300

    chore: updates database connection string

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ COPY . .
 RUN composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction --no-progress --no-suggest --no-scripts --no-plugins
 
 ENV BASE_URL=http://localhost:80
-EXPOSE 80
+EXPOSE 80 3306
--- config/db.php
+++ config/db.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql://mysql:6e879fa479780d73@dokku-mysql-sistema-de-defesas-api:3306/sistema_de_defesas_api',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=dokku-mysql-sistema-de-defesas-api;dbname=sistema_de_defesas_api;port=3306',
+    // 'dsn' => 'mysql://mysql:6e879fa479780d73@dokku-mysql-sistema-de-defesas-api:3306/sistema_de_defesas_api',
+    'username' => 'mysql',
+    'password' => '6e879fa479780d73',
     'charset' => 'utf8',
 ];
